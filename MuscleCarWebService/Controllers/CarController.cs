472a3c3865a679a615e33e7a1655087a878ed157
José Ferrari <joscferrari@gmail.com>
2024-08-22 19:10:42 -0300

    Adding service layer.

--- MuscleCarWebService/Controllers/CarController.cs
+++ MuscleCarWebService/Controllers/CarController.cs
@@ -1,46 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
 using CarWebService.Models;
+using CarWebService.Services;
 
 namespace CarWebService.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class CarController : ControllerBase
-    {      
-        private readonly ILogger<CarController> _logger; 
-        private readonly CarList _carList;
+    {
+        private readonly ILogger<CarController> _logger;
+        private readonly CarService _carService;
 
-        public CarController(ILogger<CarController> logger, CarList carList)
+        public CarController(ILogger<CarController> logger, CarService carService)
         {
-            _carList = carList;
+            _carService = carService;
             _logger = logger;
         }
 
         [HttpGet(Name = "GetCar")]
         public List<Car> Get()
         {
-            return _carList.Cars.ToList();
+            return _carService.GetCars();
         }
 
         [HttpPost(Name = "PostCar")]
         public void Post(string model, string year, string maker, string summary, string category)
         {
-            Car newCar = new Car { Model =  model, Year = year, Maker = maker, Summary = summary, Category = category };
-            _carList.Cars.Add(newCar);
-        }
-
-        [HttpDelete(Name = "DeleteCar")]
-        public void Delete(string model)
-        {
-            Car selectedCar = _carList.Cars.Find(x => x.Model == model);
-            _carList.Cars.Remove(selectedCar);
+            Car newCar = new Car { Model = model, Year = year, Maker = maker, Summary = summary, Category = category };
+            _carService.AddCar(newCar);
         }
 
         [HttpPut(Name = "UpdateCar")]
         public void Update(string model, string newName)
         {
-            Car selectedCar = _carList.Cars.Find(x => x.Model == model);
-            selectedCar.Model = newName;
+            _carService.UpdateCar(model, newName);
+        }
+
+        [HttpDelete(Name = "DeleteCar")]
+        public void Delete(string model)
+        {
+            _carService.DeteleCar(model);
         }
     }
 }
---
+++ MuscleCarWebService/Services/CarService.cs
@@ -0,0 +1,48 @@
+﻿using CarWebService.Models;
+
+namespace CarWebService.Services
+{
+    public class CarService
+    {
+        private readonly CarList _carList;
+        public CarService(CarList carList)
+        {
+            _carList = carList ?? throw new ArgumentNullException(nameof(carList));
+        }
+
+        public List<Car> GetCars()
+        {
+            return _carList.Cars.ToList();
+        }
+        public Car AddCar(Car newCar)
+        {
+            _carList.Cars.Add(newCar);
+
+            return newCar;
+        }
+        public Car UpdateCar(string model, string newName)
+        {
+            Car selectedCar = _carList.Cars.Find(x => x.Model == model);
+            if (selectedCar != null)
+            {
+                selectedCar.Model = newName;
+
+                return selectedCar;
+            }
+
+            throw new KeyNotFoundException($"Car with model '{model}' not found.");
+        }
+
+        public Car DeteleCar(string model)
+        {
+            Car selectedCar = _carList.Cars.Find(x => x.Model == model);
+            if (selectedCar != null)
+            {
+                _carList.Cars.Remove(selectedCar);
+                return selectedCar;
+            }
+
+            throw new KeyNotFoundException($"Car with model '{model}' not found.");
+        }
+    }
+}
